62a6bb8dc8ec8cb44fe3f79976a86147fb430c4a
Greg <gmerrill03@gmail.com>
2019-11-16 13:29:19 -0800

    Migrate relevant state to add-admin page

--- react-frontend/src/pages/AddAdmin.js
+++ react-frontend/src/pages/AddAdmin.js
@@ -3,6 +3,19 @@ import './scss/AddAdmin.scss'
 import BtnGeneric from '../components/BtnGeneric';
 
 class AddAdmin extends Component {
+
+  state = {
+    // these are not set in stone, just using these for a quick mock up
+    // indicates if user is logged in
+    isAuthenticated: true,
+    // ensure that the type of user is Admin, otherwise redirect user
+    userType: 'Admin',
+    // Add new user button has been clicked to reveal email input field
+    referralInputMode: true,
+    email: '',
+    pw: ''
+  }
+
   render() {
     return (
       <div className='AddAdmin'>
