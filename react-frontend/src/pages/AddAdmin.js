66ccaafcb362932ea5a6f926ec74a05e8b203c20
Greg <gmerrill03@gmail.com>
2019-11-16 16:03:41 -0800

    Add new-admin button functionality

--- react-frontend/src/pages/AddAdmin.js
+++ react-frontend/src/pages/AddAdmin.js
@@ -5,17 +5,58 @@ import BtnGeneric from '../components/BtnGeneric';
 class AddAdmin extends Component {
 
   state = {
-    // these are not set in stone, just using these for a quick mock up
+    // these state properties are not set in stone
+    // just using these to make a quick mock up
     // Add new user button has been clicked to reveal email input field
-    referralInputMode: true,
+    inputMode: false,
     email: '',
     pw: '',
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  showEmailInput = e => {
+    e.preventDefault();
+    this.setState({
+      inputMode: true
+    })
+  }
+
   render() {
+
+    const content = this.state.inputMode ? (
+      <>
+        <label htmlFor="add-admin-email">
+          Enter the e-mail address of the person you would like to invite as an admin.
+        </label>
+        <input
+          id="add-admin-email"
+          type='email'
+          value={this.state.email}
+          name='email'
+          placeholder='email address'
+          onChange={this.handleChange}
+        />
+      </>
+    ) : (
+      <BtnGeneric
+        btnText='add new admin'
+        btnType='success'
+      />
+    )
+
     return (
       <div className='AddAdmin'>
-        <BtnGeneric btnName='add new user' btnType='success' />
+        <form 
+          className='add-admin-form' 
+          onSubmit={this.showEmailInput}
+        >
+          {content}
+        </form>
       </div>
     );
   }
